5e8cdbf37acb9015e2521602f39f133b60ece1cc
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2023-02-28 18:20:33 +0100

    Ajout du squelette de SymboleFonction
    # Détails :
     - Ajout du squelette de la classe avec quelques fonctions temporaires
    
    # Remarques :
     - Les fonctions ne font pas ce qui est attendu d'elles

---
+++ src/zoot/tds/symboles/SymboleFonction.java
@@ -0,0 +1,14 @@
+package zoot.tds.symboles;
+
+import zoot.arbre.expressions.Identifiable;
+import zoot.tds.Type;
+
+public class SymboleFonction extends Symbole {
+    public SymboleFonction(Type type) {
+        super(type);
+    }
+
+    void decorer(Identifiable a) {
+        // TODO mettre la bonne signature
+    }
+}
