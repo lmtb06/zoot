ad908b11b24ba9bd7b10c6f0e0567a4a5c128074
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2023-02-28 18:19:32 +0100

    Ajout du squelette d' EtatEnCoursAnalyseSyntaxique
    # Détails :
     - Ajout du squelette de la classe avec ses fonctions
    
    # Remarques :
     - Les fonctions ne font pas ce qui est attendu d'elles

---
+++ src/zoot/tds/etats/EtatEnCoursAnalyseSyntaxique.java
@@ -0,0 +1,31 @@
+package zoot.tds.etats;
+
+import zoot.tds.TDS;
+import zoot.tds.entrees.Entree;
+import zoot.tds.symboles.Symbole;
+
+public class EtatEnCoursAnalyseSyntaxique extends EtatAnalyse {
+    public EtatEnCoursAnalyseSyntaxique(TDS tds) {
+        super(tds);
+    }
+
+    public void ajouter(Entree entree, Symbole symbole) {
+        // TODO
+    }
+
+    public void augmenterTailleZoneVariables(int nbOctets) {
+        // TODO
+    }
+
+    public void entreeBloc() {
+        // TODO
+    }
+
+    public void sortieBloc() {
+        // TODO
+    }
+
+    public void allerEtatSuivant() {
+
+    }
+}
