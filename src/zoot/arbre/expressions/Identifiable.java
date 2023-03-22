34a6d71768400e9ad28f93727c1a1b0a8d12e766
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2023-03-22 02:09:34 +0100

    Modification du package expressions en accord avec le diagramme de classes
    # Détails :
     - suppression de setSymbole
     - Implémentation fonctionnelle
     - Correction des verifier
     - toMIPS fonctionnels

--- src/zoot/arbre/expressions/Identifiable.java
+++ src/zoot/arbre/expressions/Identifiable.java
@@ -1,5 +1,7 @@
 package zoot.arbre.expressions;
 
+import zoot.tds.TDS;
+import zoot.tds.Type;
 import zoot.tds.entrees.Entree;
 import zoot.tds.symboles.Symbole;
 
@@ -12,5 +14,21 @@ public abstract class Identifiable extends Expression {
         this.entree = e;
     }
 
-    public abstract void setSymbole(Symbole s);
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Type getType() {
+        return symbole.getType();
+    }
+
+    /**
+     * {@inheritDoc}
+     * Récupère le {@link Symbole} associé et ajoute une exception au gestionnaire d'exception
+     * s'il n'y a pas de {@link Symbole} associé à son {@link Entree}.
+     */
+    @Override
+    public void verifier() {
+        symbole = TDS.getInstance().identifier(entree);
+    }
 }
