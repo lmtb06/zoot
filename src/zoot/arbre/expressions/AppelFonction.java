e915a95e4bb2fe63026c738f73ff1d5475b2221e
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2023-03-27 16:22:02 +0200

    Refactoring de Identifiable, de AppelFonction et de Variable
    # Détails:
     - Modification du diagramme de classe (choix d'utiliser un Iterator plutôt qu'une collection) pour la fonction AppelFonction.getParametres
     - Refactoring en accord avec le diagramme de classes
     - Mise à jour de la grammaire pour prendre en compte ces changements

--- src/zoot/arbre/expressions/AppelFonction.java
+++ src/zoot/arbre/expressions/AppelFonction.java
@@ -6,7 +6,13 @@ import zoot.exceptions.EntreeNonTrouveException;
 import zoot.exceptions.FonctionNonDeclarerException;
 import zoot.exceptions.GestionnaireExceptionsSemantiques;
 import zoot.exceptions.LigneDecorator;
+import zoot.tds.TDS;
+import zoot.tds.Type;
 import zoot.tds.entrees.EntreeFonction;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class AppelFonction extends Identifiable {
     private int niveauImbrication;
@@ -14,9 +20,13 @@ public class AppelFonction extends Identifiable {
     private final int tailleZoneParametres;
     private String etiquette;
 
-    public AppelFonction(EntreeFonction e, int n) {
-        super(e, n);
-        tailleZoneParametres = e.getTypeParametres().size() * 4;
+    private final ArrayList<Expression> parametres;
+
+    public AppelFonction(String identifiant, int n, Expression... parametres) {
+        super(identifiant, n);
+        this.parametres = new ArrayList<>(parametres.length);
+        Collections.addAll(this.parametres, parametres);
+        tailleZoneParametres = parametres.length;
     }
 
     public int getTailleZoneParametres() {
@@ -64,13 +74,28 @@ public class AppelFonction extends Identifiable {
     }
 
     /**
+     * @return les paramètres de l'appel de fonction
+     */
+    public Iterator<Expression> getParametres() {
+        return parametres.iterator();
+    }
+
+    /**
      * {@inheritDoc}
      * Décore la variable pour la génération de code grâce à son symbole.
      */
     @Override
     public void verifier() {
+        ArrayList<Type> typesParametres = new ArrayList<>(parametres.size());
+
+        for (Expression e : parametres) {
+            typesParametres.add(e.getType());
+        }
+
+        EntreeFonction entree = new EntreeFonction(identifiant, typesParametres);
+
         try {
-            super.verifier();
+            symbole = TDS.getInstance().identifier(entree);
             symbole.decorer(this);
         } catch (EntreeNonTrouveException e) {
             GestionnaireExceptionsSemantiques.getInstance()
